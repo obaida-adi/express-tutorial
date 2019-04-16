09b4c00ae5cbf910b4e5e9025dea9922b80dbab7
obaida-adi <obaida.adi@hotmail.com>
2019-04-16 15:56:48 -0400

    installed nodemon for dev

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
